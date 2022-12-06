d204f8b77f10f4d7c39fdc6ff3a4198ef1aa4e16
cartkid <ericlevicky@gmail.com>
2022-12-06 07:34:24 -0500

    day 6 complete

--- tests/year_2022/day_7/code_test.py
+++ tests/year_2022/day_7/code_test.py
@@ -1,25 +1,25 @@
-import src.year_2022.day_6.code as day
+import src.year_2022.day_7.code as day
 
 
 def test_some_function():
-    expected = 7
+    expected = 0
     result = day.some_function("test.txt")
     assert result == expected
 
 
 def test_answer_some_function():
-    expected = 1909
+    expected = 0
     result = day.some_function("input.txt")
     assert result == expected
 
 
 def test_part_two_some_function():
-    expected = 19
+    expected = 0
     result = day.some_function("test.txt", True)
     assert result == expected
 
 
 def test_answer_part_two_some_function():
-    expected = 3380
+    expected = 0
     result = day.some_function("input.txt", True)
     assert result == expected
