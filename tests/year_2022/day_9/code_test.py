cd35050448a33d83607235c89a704ff448bdcad8
cartkid <ericlevicky@gmail.com>
2022-12-07 08:49:10 -0500

    day 9 prep

---
+++ tests/year_2022/day_9/code_test.py
@@ -0,0 +1,25 @@
+import src.year_2022.day_9.code as day
+
+
+def test_some_function():
+    expected = 0
+    result = day.some_function("test.txt")
+    assert result == expected
+
+
+def test_answer_some_function():
+    expected = 0
+    result = day.some_function("input.txt")
+    assert result == expected
+
+
+def test_part_two_some_function():
+    expected = 0
+    result = day.some_function("test.txt", True)
+    assert result == expected
+
+
+def test_answer_part_two_some_function():
+    expected = 0
+    result = day.some_function("input.txt", True)
+    assert result == expected
