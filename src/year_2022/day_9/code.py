d7b3db8edcc9589b4efeb1dce32096abd047e700
cartkid <ericlevicky@gmail.com>
2022-12-09 11:14:40 -0500

    day 9 complete

--- src/year_2022/day_9/code.py
+++ src/year_2022/day_9/code.py
@@ -1,4 +1,82 @@
 import os
+import math
+
+
+class rope:
+    def __init__(
+        self,
+        snake_length: int = 2,
+    ):
+        self.snake_length = snake_length
+        start: tuple[int, int] = (0, 0)
+        self.tail_coordinates: list[tuple[int, int]] = [start]
+        self.snake_coordinates: dict[int, tuple[int, int]] = {}
+
+        for i in range(0, self.snake_length):
+            self.snake_coordinates[i] = start
+
+    def move_step(self, direction_to_move: str):
+        direction_to_move = direction_to_move.upper()
+        self.__move_head_step(direction_to_move=direction_to_move)
+        for i in range(1, self.snake_length):
+            self.__move_next_piece_step(piece=i)
+
+    def __move_head_step(self, direction_to_move: str):
+        self.snake_coordinates[0] = rope.get_move_result_coordinates(
+            curr_coordinates=self.snake_coordinates[0],
+            direction_to_move=direction_to_move,
+        )
+
+    def __move_next_piece_step(self, piece: int):
+        if self.is_piece_close_enough(piece=piece) is False:
+            following = self.snake_coordinates[piece - 1]
+            current = self.snake_coordinates[piece]
+
+            temp_x = following[0] - current[0]
+            new_coord_x = (
+                temp_x if abs(temp_x) <= 1 else math.floor(temp_x / 2)
+            ) + current[0]
+            temp_y = following[1] - current[1]
+            new_coord_y = (
+                temp_y if abs(temp_y) <= 1 else math.floor(temp_y / 2)
+            ) + current[1]
+
+            self.snake_coordinates[piece] = (new_coord_x, new_coord_y)
+
+            if (
+                piece + 1 == self.snake_length
+                and self.snake_coordinates[piece] not in self.tail_coordinates
+            ):
+                self.tail_coordinates.append(self.snake_coordinates[piece])
+
+    @staticmethod
+    def get_move_result_coordinates(
+        curr_coordinates: tuple[int, int],
+        direction_to_move: str,
+    ) -> tuple[int, int]:
+        return_me: tuple[int, int] = curr_coordinates
+        if direction_to_move == "U":
+            return_me = (curr_coordinates[0], curr_coordinates[1] + 1)
+        elif direction_to_move == "D":
+            return_me = (curr_coordinates[0], curr_coordinates[1] - 1)
+        elif direction_to_move == "L":
+            return_me = (curr_coordinates[0] - 1, curr_coordinates[1])
+        elif direction_to_move == "R":
+            return_me = (curr_coordinates[0] + 1, curr_coordinates[1])
+
+        return return_me
+
+    def is_piece_close_enough(self, piece: int) -> bool:
+        if (
+            abs(self.snake_coordinates[piece - 1][0] - self.snake_coordinates[piece][0])
+            <= 1
+            and abs(
+                self.snake_coordinates[piece - 1][1] - self.snake_coordinates[piece][1]
+            )
+            <= 1
+        ):
+            return True
+        return False
 
 
 def some_function(file_name: str = "input.txt", part_two: bool = False) -> int:
@@ -7,22 +85,29 @@ def some_function(file_name: str = "input.txt", part_two: bool = False) -> int:
     file = open(os.path.join(directory, file_name), "r")
     all_lines = file.readlines()
 
-    for i, line in enumerate(all_lines):
-        values_only = line.strip("\n")
-        after_count: int = 4 if part_two is False else 14
-        for j, char in enumerate(values_only):
-            if len(values_only) < j + after_count:
-                return -1
-            lst: list[str] = []
-            found_it: bool = False
-            for k in range(after_count):
-                if values_only[j + k] in lst:
-                    break
-                lst.append(values_only[j + k])
-                if k == after_count - 1:
-                    found_it = True
-                    return_me = j + after_count
-            if found_it is True:
-                break
+    snake_length: int = 2 if part_two is False else 10
+    curr_rope: rope = rope(snake_length=snake_length)
+
+    for line in all_lines:
+        values_only = line.strip("\n").split(" ")
+        direction_to_move = values_only[0]
+        number_to_move = int(values_only[1])
+
+        for i in range(0, number_to_move):
+            curr_rope.move_step(direction_to_move=direction_to_move)
+
+    return_me = len(curr_rope.tail_coordinates)
+
+    # to print the tail
+    all_y = [x[1] for x in curr_rope.tail_coordinates]
+    all_x = [x[0] for x in curr_rope.tail_coordinates]
+    for y in range(max(all_y), min(all_y) - 1, -1):
+        debug = ""
+        for x in range(min(all_x), max(all_x) + 1):
+            if (x, y) in curr_rope.tail_coordinates:
+                debug += "#"
+            else:
+                debug += "."
+        print(debug)
 
     return return_me
--- tests/year_2022/day_9/code_test.py
+++ tests/year_2022/day_9/code_test.py
@@ -1,25 +1,203 @@
 import src.year_2022.day_9.code as day
+import pytest
 
 
 def test_some_function():
-    expected = 0
+    expected = 13
     result = day.some_function("test.txt")
     assert result == expected
 
 
 def test_answer_some_function():
-    expected = 0
+    expected = 6384
     result = day.some_function("input.txt")
     assert result == expected
 
 
+def test_diagnal_u_some_function():
+    rope = day.rope(3)
+    rope.snake_coordinates[0] = (1, 1)
+    rope.snake_coordinates[1] = (0, 0)
+    rope.snake_coordinates[2] = (-1, 0)
+    rope.move_step("U")
+
+    assert rope.snake_coordinates[0] == (1, 2)
+    assert rope.snake_coordinates[1] == (1, 1)
+    assert rope.snake_coordinates[2] == (0, 1)
+
+
+def test_diagnal_d_some_function():
+    rope = day.rope(3)
+    rope.snake_coordinates[0] = (0, 0)
+    rope.snake_coordinates[1] = (1, 1)
+    rope.snake_coordinates[2] = (2, 1)
+    rope.move_step("D")
+
+    assert rope.snake_coordinates[0] == (0, -1)
+    assert rope.snake_coordinates[1] == (0, 0)
+    assert rope.snake_coordinates[2] == (1, 0)
+
+
+def test_diagnal_l_some_function():
+    rope = day.rope(3)
+    rope.snake_coordinates[0] = (0, 0)
+    rope.snake_coordinates[1] = (1, 1)
+    rope.snake_coordinates[2] = (2, 1)
+    rope.move_step("L")
+
+    assert rope.snake_coordinates[0] == (-1, 0)
+    assert rope.snake_coordinates[1] == (0, 0)
+    assert rope.snake_coordinates[2] == (1, 0)
+
+    assert len(rope.tail_coordinates) == 2
+
+
+def test_diagnal_r_some_function():
+    rope = day.rope(10)
+    rope.snake_coordinates[0] = (-5, 0)
+    rope.snake_coordinates[1] = (-6, 1)
+    rope.snake_coordinates[2] = (-7, 1)
+    rope.snake_coordinates[3] = (-7, 1)
+    rope.snake_coordinates[4] = (-7, 1)
+    rope.snake_coordinates[5] = (-7, 1)
+    rope.snake_coordinates[6] = (-7, 1)
+    rope.snake_coordinates[7] = (-7, 1)
+    rope.snake_coordinates[8] = (-7, 1)
+    rope.snake_coordinates[9] = (-7, 1)
+    rope.move_step("R")
+
+    assert rope.snake_coordinates[0] == (-4, 0)
+    assert rope.snake_coordinates[1] == (-5, 0)
+    assert rope.snake_coordinates[2] == (-6, 0)
+    assert rope.snake_coordinates[3] == (-7, 1)
+    assert rope.snake_coordinates[4] == (-7, 1)
+    assert rope.snake_coordinates[5] == (-7, 1)
+    assert rope.snake_coordinates[6] == (-7, 1)
+    assert rope.snake_coordinates[7] == (-7, 1)
+    assert rope.snake_coordinates[8] == (-7, 1)
+    assert rope.snake_coordinates[9] == (-7, 1)
+
+    assert len(rope.tail_coordinates) == 1
+
+
+# ..........................
+# ........H1234.............
+# ............5.............
+# ............6.............
+# ............7.............
+# ............8.............
+# ............9.............
+# ..........................
+# ..........................
+# ...........s..............
+# ..........................
+# ..........................
+# ..........................
+# ..........................
+# ..........................
+
+# .........2345.............
+# ........1...6.............
+# ........H...7.............
+# ............8.............
+# ............9.............
+# ..........................
+# ..........................
+# ...........s..............
+# ..........................
+# ..........................
+# ..........................
+# ..........................
+# ..........................
+
+
+def test_diagnal_d3_some_function():
+    rope = day.rope(10)
+    rope.snake_coordinates[0] = (0, 0)
+    rope.snake_coordinates[1] = (1, 0)
+    rope.snake_coordinates[2] = (2, 0)
+    rope.snake_coordinates[3] = (3, 0)
+    rope.snake_coordinates[4] = (4, 0)
+    rope.snake_coordinates[5] = (4, -1)
+    rope.snake_coordinates[6] = (4, -2)
+    rope.snake_coordinates[7] = (4, -3)
+    rope.snake_coordinates[8] = (4, -4)
+    rope.snake_coordinates[9] = (4, -5)
+    rope.move_step("D")
+    rope.move_step("D")
+    rope.move_step("D")
+
+    assert rope.snake_coordinates[0] == (0, -3)
+    assert rope.snake_coordinates[1] == (0, -2)
+    assert rope.snake_coordinates[2] == (1, -1)
+    assert rope.snake_coordinates[3] == (2, -1)
+    assert rope.snake_coordinates[4] == (3, -1)
+    assert rope.snake_coordinates[5] == (4, -1)
+    assert rope.snake_coordinates[6] == (4, -2)
+    assert rope.snake_coordinates[7] == (4, -3)
+    assert rope.snake_coordinates[8] == (4, -4)
+    assert rope.snake_coordinates[9] == (4, -5)
+
+    assert len(rope.tail_coordinates) == 1
+
+
+def test_diagnal_r17_some_function():
+    rope = day.rope(10)
+    rope.snake_coordinates[0] = (0, -3)
+    rope.snake_coordinates[1] = (0, -2)
+    rope.snake_coordinates[2] = (1, -1)
+    rope.snake_coordinates[3] = (2, -1)
+    rope.snake_coordinates[4] = (3, -1)
+    rope.snake_coordinates[5] = (4, -1)
+    rope.snake_coordinates[6] = (4, -2)
+    rope.snake_coordinates[7] = (4, -3)
+    rope.snake_coordinates[8] = (4, -4)
+    rope.snake_coordinates[9] = (4, -5)
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+    rope.move_step("R")
+
+    assert rope.snake_coordinates[0] == (17, -3)
+    assert rope.snake_coordinates[1] == (16, -3)
+    assert rope.snake_coordinates[2] == (15, -3)
+    assert rope.snake_coordinates[3] == (14, -3)
+    assert rope.snake_coordinates[4] == (13, -3)
+    assert rope.snake_coordinates[5] == (12, -3)
+    assert rope.snake_coordinates[6] == (11, -3)
+    assert rope.snake_coordinates[7] == (10, -3)
+    assert rope.snake_coordinates[8] == (9, -3)
+    assert rope.snake_coordinates[9] == (8, -3)
+
+    assert len(rope.tail_coordinates) == 5
+
+
 def test_part_two_some_function():
-    expected = 0
+    expected = 1
     result = day.some_function("test.txt", True)
     assert result == expected
 
 
+def test_part_two_test2_some_function():
+    expected = 36
+    result = day.some_function("test2.txt", True)
+    assert result == expected
+
+
 def test_answer_part_two_some_function():
-    expected = 0
+    expected = 2734
     result = day.some_function("input.txt", True)
     assert result == expected
