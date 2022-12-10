f18c87c57eb3ed6d0c915025a6244263c954243e
cartkid <ericlevicky@gmail.com>
2022-12-10 09:55:18 -0500

    day 10 complete

--- src/year_2022/day_10/code.py
+++ src/year_2022/day_10/code.py
@@ -1,4 +1,20 @@
 import os
+import math
+
+
+def print_screen(screen: list[list[str]]):
+    for r in screen:
+        print("".join(r))
+
+
+def get_curr_sprite(curr_x: int) -> list[str]:
+    return_me: list[str] = []
+    for i in range(0, 40):
+        if i in [curr_x - 1, curr_x, curr_x + 1]:
+            return_me.append("#")
+        else:
+            return_me.append(" ")
+    return return_me
 
 
 def some_function(file_name: str = "input.txt", part_two: bool = False) -> int:
@@ -7,22 +23,59 @@ def some_function(file_name: str = "input.txt", part_two: bool = False) -> int:
     file = open(os.path.join(directory, file_name), "r")
     all_lines = file.readlines()
 
+    cycle_consumption: dict[str, int] = {"noop": 1, "addx": 2}
+    curr_x: int = 1  # starts at 1
+    curr_cycle: int = 0
+    important_cycles: list[int] = [20, 60, 100, 140, 180, 220]
+    curr_important_cycle_index: int = 0
+    important_cycle_x_values: list[int] = []
+
+    screen: list[list[str]] = []
+    # make a blank screen
+    for r in range(0, 6):
+        screen.append([])
+        for c in range(0, 40):
+            screen[r].append(".")
+
     for i, line in enumerate(all_lines):
-        values_only = line.strip("\n")
-        after_count: int = 4 if part_two is False else 14
-        for j, char in enumerate(values_only):
-            if len(values_only) < j + after_count:
-                return -1
-            lst: list[str] = []
-            found_it: bool = False
-            for k in range(after_count):
-                if values_only[j + k] in lst:
-                    break
-                lst.append(values_only[j + k])
-                if k == after_count - 1:
-                    found_it = True
-                    return_me = j + after_count
-            if found_it is True:
-                break
+        values_only = line.strip("\n").split(" ")
+
+        curr_sprite = get_curr_sprite(curr_x=curr_x)
+
+        operation: str = values_only[0]
+        amount_to_add: int = 0
+        if operation == "addx":
+            amount_to_add = int(values_only[1])
+        cycles_consumed = cycle_consumption[operation]
+        start = curr_cycle
+        end = curr_cycle + cycles_consumed
+        prior_cycle_count = curr_cycle
+        for c in range(start, end):
+            column = c % 40
+            row = math.floor(c / 40)
+            screen[row][column] = curr_sprite[column]
+            curr_cycle += 1
+
+        if part_two is False:
+            if (
+                prior_cycle_count
+                < important_cycles[curr_important_cycle_index]
+                <= curr_cycle
+            ):
+                important_cycle_x_values.append(
+                    important_cycles[curr_important_cycle_index] * curr_x
+                )
+                if len(important_cycles) - 1 == curr_important_cycle_index:
+                    pass
+                else:
+                    curr_important_cycle_index += 1
+
+        curr_x += amount_to_add
+
+    if part_two is False:
+        return_me = sum(important_cycle_x_values)
+    else:
+        print_screen(screen)
+        return_me = -1
 
     return return_me
--- tests/year_2022/day_11/code_test.py
+++ tests/year_2022/day_11/code_test.py
@@ -1,25 +1,25 @@
-import src.year_2022.day_10.code as day
+import src.year_2022.day_11.code as day
 
 
 def test_some_function():
-    expected = 13140
+    expected = 0
     result = day.some_function("test.txt")
     assert result == expected
 
 
 def test_answer_some_function():
-    expected = 13220
+    expected = 0
     result = day.some_function("input.txt")
     assert result == expected
 
 
 def test_part_two_some_function():
-    expected = -1
+    expected = 0
     result = day.some_function("test.txt", True)
     assert result == expected
 
 
 def test_answer_part_two_some_function():
-    expected = -1
+    expected = 0
     result = day.some_function("input.txt", True)
     assert result == expected
